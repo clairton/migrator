a5fd597107bbc5ed0ea44e45b6008867ce859d03
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2016-06-13 08:36:18 -0300

    Run all tests

--- src/test/java/br/eti/clairton/migrator/InserterTest.java
+++ src/test/java/br/eti/clairton/migrator/InserterTest.java
@@ -1,5 +1,11 @@
 package br.eti.clairton.migrator;
 
+import static java.lang.System.setProperty;
+import static java.lang.Thread.currentThread;
+import static java.sql.DriverManager.getConnection;
+import static java.util.Arrays.asList;
+import static java.util.Collections.enumeration;
+import static org.jboss.vfs.VFS.getChild;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -9,25 +15,21 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarInputStream;
 
-import org.jboss.vfs.VFS;
 import org.jboss.vfs.VirtualFile;
 import org.junit.Before;
 import org.junit.Test;
 
 public class InserterTest {
 	static {
-		System.setProperty(Config.DROP, "true");
-		System.setProperty(Config.POPULATE, "true");
+		setProperty(Config.DROP, "true");
+		setProperty(Config.POPULATE, "true");
 	}
 
 	private Connection connection;
@@ -35,29 +37,33 @@ public class InserterTest {
 	@Before
 	public void setUp() throws Exception {
 		final String url = "jdbc:hsqldb:file:target/database/migrator2;hsqldb.lock_file=false;shutdown=true;create=true";
-		connection = DriverManager.getConnection(url, "sa", "");
+		connection = getConnection(url, "sa", "");
 		connection.setAutoCommit(true);
 		try {
 			connection.createStatement().execute("DELETE FROM aplicacoes");
-		} catch (Exception e) {
-
-		}
+		} catch (Exception e) {}
 	}
 
 	@Test
 	public void testInsideJar() throws Exception {
-		final ClassLoader classLoader = new ClassLoader(getClass()
-				.getClassLoader()) {
+		final ClassLoader classLoader = new ClassLoader(getClass().getClassLoader()) {
 			@Override
-			public Enumeration<URL> getResources(String name)
-					throws IOException {
+			public Enumeration<URL> getResources(String name) throws IOException {
 				// jar:file:/home/maxicreditosc/.m2/repository/br/com/maxicredito/auth-api/1.0.0-SNAPSHOT/auth-api-1.0.0-SNAPSHOT.jar!/datasets/
 				final File jar = new File("src/test/resources/test.jar");
-				final String file = "jar:file:" + jar.getAbsolutePath()
-						+ "!/datasets/";
-				final URL url = new URL(file);
-				final Collection<URL> c = Arrays.asList(url);
-				return Collections.enumeration(c);
+				final Collection<URL> c;
+				if(name.endsWith("xml")){
+					final String file = "jar:file:" + jar.getAbsolutePath() + "!/db/changelogs/changelog-main.xml";
+					final URL url = new URL(file);
+					c = asList(url);						
+				}else if(name.endsWith("xsd")){
+					return super.getResources(name);					
+				}else{
+					final String file = "jar:file:" + jar.getAbsolutePath() + "!/datasets/";
+					final URL url = new URL(file);
+					c = asList(url);
+				}
+				return enumeration(c);
 			}
 		};
 		run(classLoader);
@@ -71,11 +77,18 @@ public class InserterTest {
 				try {
 					// file:/home/maxicreditosc/.m2/repository/br/com/maxicredito/auth-api/1.0.0-SNAPSHOT/auth-api-1.0.0-SNAPSHOT.jar/datasets/
 					final File jar = new File("src/test/resources/test.jar");
-					final String file = "jar:file:" + jar.getAbsolutePath() + "!/datasets/";
-					final URL url = new URL(file);
-					final VirtualFile vf = VFS.getChild(url.toURI());
-					final Collection<URL> c = Arrays.asList(vf.toURL());
-					return Collections.enumeration(c);
+					final Collection<URL> c;
+					if(name.endsWith("xml")){
+						final String file = "jar:file:" + jar.getAbsolutePath() + "!/db/changelogs/changelog-main.xml";
+						final URL url = new URL(file);
+						c = asList(url);						
+					}else{
+						final String file = "jar:file:" + jar.getAbsolutePath() + "!/datasets/";
+						final URL url = new URL(file);
+						final VirtualFile vf = getChild(url.toURI());
+						c = asList(vf.toURL());
+					}
+					return enumeration(c);
 				} catch (Exception e) {
 					throw new IOException(e);
 				}
@@ -98,7 +111,7 @@ public class InserterTest {
 		final FileInputStream fis = new FileInputStream(jar);
 		final JarInputStream jarFile = new JarInputStream(fis);
 		final Inserter inserter = new Inserter();
-		final List<URL> list = inserter.loadJar(jarFile, Thread.currentThread().getContextClassLoader(), "datasets");
+		final List<URL> list = inserter.loadJar(jarFile, currentThread().getContextClassLoader(), "datasets");
 		assertEquals(1, list.size());
 	}
 
